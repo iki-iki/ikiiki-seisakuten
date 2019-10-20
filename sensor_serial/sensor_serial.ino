17fd58d5b98e2bc90688f99d97e4e43920d83a7f
maro525 <hdmrf.fjnm@gmail.com>
2019-10-20 17:15:33 +0900

    fixed

--- sensor_serial/sensor_serial.ino
+++ sensor_serial/sensor_serial.ino
@@ -1,15 +1,21 @@
 int photo_pin = 1;
 
-
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
 }
 
 void loop() {
     int s = analogRead(photo_pin);
-    uint8_t val = (uint8_t)(s / 4);
-    // Serial.println(s);
+    // uint8_t val = (uint8_t)(s/4);
+    String send = String(s);
+    // String send = "hello world";
+    Serial.println(send);
     // Serial.print(" ");
     // Serial.println(val);
-    Serial.write(val + "f");
+    // byte msg = byte(s);
+    // Serial.write(msg);
+    // Serial.write("\n");
+    // Serial.write("hello\n");
+
+    delay(10);
 }
