01feae83daf8f6883da7428cfa1fa64b34c6c8ca
maro525 <hdmrf.fjnm@gmail.com>
2019-11-01 13:17:08 +0900

    update python

--- send_serial.py
+++ send_serial.py
@@ -1,56 +1,59 @@
 import time
 import grovepi
 import serial
+from windmill_calc import Windmill_Calc
 
 TTY = "/dev/ttyUSB0"
 BAUD = 115200 
 
 # HIGH : black 
 # LOW : white 
+# switchしたいときだけシグナルを送る
 
 class Send_Serial:
     def __init__(self, tty, baud):
+        # init serial
         self.ser = serial.Serial(tty, baud, timeout=0.1)
+        # init sensor settings
         self.sensor_pin = 4
         grovepi.pinMode(self.sensor_pin, "INPUT")
         self.prev = 0
+        # init variables
         self.stop_time = time.time()
         self.stop_duration = 3
-        self.state_change = True
+        self.state_change = False
+        # init calculator
+        self.WC = Windmill_Calc()
     
     def main(self):
         while True:
             s = grovepi.digitalRead(self.sensor_pin)
-            v = self.judge(s)
-            self.send_msg(v)            
+            v = self.judge_wind(s)
+            # 1 : start 0: stop -1 : keep
+            msg = self.WC.calc(v)            
+            self.send_msg(msg)
             time.sleep(.01)
 
-    def send_msg(self, value):
-        if value is 1:
-            msg = b"a"
-        elif value is 0:
-            msg = b"b"
-        elif value is -1:
-            return
+    def send_msg(self, msg):
+        print("[INFO] send ...", msg)
         self.ser.write(msg)
 
-    def judge(self, s):
-        print(self.prev, " -> ", s)
-        if s != self.prev:
+    def judge_wind(self, s):
+        # print(self.prev, " -> ", s)
+        if s != self.prev: # get state change
             v = 1
-            print("start")
-            self.state_change = 1
-        else:
-            v = -1
-            if self.state_change == 1:
+            print("[INFO]start wind")
+            if s is 0:
                 self.stop_time = time.time()
+                print("[INFO]Set Stop Time")
+        else: # if state not changes
+            v = -1
             duration = time.time() - self.stop_time
-            if duration > self.stop_duration + 0.5:
-                v = -1
-            elif duration > self.stop_duration:
+            if s is 0 and duration > self.stop_duration + 0.5:
                 v = 0
-                print("stop")
+                print("[INFO]stop wind")
             self.state_change = 0
+        # record current state
         self.prev = s 
         return v
         
---
+++ windmill_calc.py
@@ -0,0 +1,53 @@
+from queue import Queue
+
+class Windmill_Calc:
+    def __init__(self):
+        self.windmill_num = 3
+        # init winds value
+        self.distance = [2, 4, 6] 
+        self.last_signal = 0 
+        self.winds = []
+        self.last_message = []
+        for i in range(len(self.windmill_num)):
+            self.winds[i] = Queue()
+            self.last_message[i] = -1
+            
+    def get_msg(self, signal):
+        self.calc(signal)
+        msg = []
+        for i in range(self.windmill_num):
+            s = self.winds[i].get()
+            if self.last_signal is s:
+                msg[i] = -1
+            elif self.last_signal is 1 and s is 0:
+                msg[i] = 0
+            elif self.last_signal is 0 and s is 1:
+                msg[i] = 1
+        strmsg = self.array_to_str(msg)
+        print("[INFO]Send Msg ...", strmsg)
+        return strmsg 
+
+    def array_to_str(self, a):
+        s = b""
+        for i in range(self.windmill_num):
+            if a[i] is 1:
+                s += b"a"
+            elif a[i] is 0:
+                s += b"b"
+            elif a[i] is -1:
+                s += b"c"
+        return s
+
+    
+    # 1 : start, 0: stop, -1: keep
+    def calc(self, signal):
+        if signal is -1:
+            for k in range(self.windmill_num):
+                self.winds[k].put(self.last_signal)
+            return
+
+        for i in range(self.windmill_num):
+            for _ in range(self.distance[i]):
+                self.winds[i].put(int(not(signal)))
+            self.winds[i].put(signal)
+        self.last_signal = signal
