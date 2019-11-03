5fea8d7aee8d811897da69c04fc122ad2ffc581a
maro525 <hdmrf.fjnm@gmail.com>
2019-11-03 17:43:08 +0900

    signal check & test code

--- dc_control/src/dc_control.ino
+++ dc_control/src/dc_control.ino
@@ -14,18 +14,16 @@ void loop() {
     if (Serial.available() > 0){
         recv_str = Serial.readStringUntil(".");
         Serial.println(recv_str);
-        int str_len = recv_str.length() - 1;
+        int str_len = recv_str.length();
         for(unsigned int i=0; i<str_len; i++){
             String ss;
             ss = recv_str.charAt(i);
             if(!ss.compareTo("a")){
-                // winds_signal[i]=1;
                 digitalWrite(pinarray[i], 1);
-                // Serial.println(winds_signal[i]);
+                // Serial.println(pinarray[i]);
             } else if(!ss.compareTo("b")) {
-                // winds_signal[i]=0;
                 digitalWrite(pinarray[i], 0);
-                // Serial.println(winds_signal[i]);
+                // Serial.println(pinarray[i]);
             }
         }
     }
--- serial_test.py
+++ serial_test.py
@@ -1,45 +1,27 @@
 import time
 import serial
 
-with serial.Serial('COM7', 115200, timeout=1) as ser:
-    
-# ser.port = "COM7"
-# ser.baud = 115200
-# ser.setDTR(False)
-# ser.open()
+print("Serial Connecting...")
+ser = serial.Serial('COM7', 115200, timeout=1)
+time.sleep(5)
 
-    time.sleep(5)
-    ser.write(b"bbbb.")
-    print("write b")
+signals = [b"aaaa", b"bbbb", b"cccc"]
+
+def send_signal(sig_ind):
+    signal = signals[sig_ind]
+    ser.write(signal)
+    print("write", signal)
     print(ser.readline())
-    time.sleep(1)
-    ser.write(b"bbbb.")
-    print("write b")
     print(ser.readline())
-    time.sleep(1)
-    ser.write(b"bbbb.")
-    print("write b")
-    print(ser.readline())
-    time.sleep(1)
-    ser.write(b"bbbb.")
-    print("write b")
     print(ser.readline())
     time.sleep(1)
 
-    time.sleep(1)
-    ser.write(b"aaaa.")
-    print("write a")
-    print(ser.readline())
+for i in range(4):
+    send_signal(0)
+    send_signal(2)
+    send_signal(2)
+    send_signal(1)
+    send_signal(2)
+    send_signal(2)
 
-
-    # stime = 1 
-    # for i in range(20):
-    #     ser.write(b"aaaa")
-    #     print("write a")
-    #     time.sleep(stime)
-    #     ser.write(b"bbbb")
-    #     print("write b")
-    #     time.sleep(stime)
-    #     ser.write(b"cccc")
-    #     print("write c")
-    #     ser.write
+ser.close()
