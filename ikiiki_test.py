9c8e4999168a12382bc797cf9f7b02dcb7ea8693
maro525 <hdmrf.fjnm@gmail.com>
2019-11-11 23:25:01 +0900

    add processing

--- ikiiki_test.py
+++ ikiiki_test.py
@@ -1,8 +1,9 @@
 import time
 import serial
 
+PORT = "COM7"
 print("Serial Connecting...")
-ser = serial.Serial('COM7', 115200, timeout=1)
+ser = serial.Serial(PORT, 115200, timeout=1)
 time.sleep(5)
 
 signals = [b"aaaa", b"bbbb", b"cccc"]
@@ -20,8 +21,9 @@ for i in range(4):
     send_signal(0)
     send_signal(2)
     send_signal(2)
-    send_signal(1)
     send_signal(2)
+    send_signal(2)
+    send_signal(1)
     send_signal(2)
 
 ser.close()
