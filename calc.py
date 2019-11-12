c8540790bf30da3cb9f5fc715ee05f96b490c190
maro525 <hdmrf.fjnm@gmail.com>
2019-11-12 22:44:37 +0900

    fixed minor bug

--- calc.py
+++ calc.py
@@ -4,15 +4,17 @@ class Windmill_Calculator:
     def __init__(self, windmills):
         self.windmills = windmills  # set windmill array pointer
         self.wind_mode = 0
+        self.sensor_info = []
 
     # bRotatings : array of sensor flag
     # no need to return value
     # set signal value for each windmill class
     def calc(self, bRotatings):
+        self.sensor_info = bRotatings
         for i in range(len(self.windmills)):
-            checkpins = self.windmills[i].get_read_pins()
+            checkpins = self.windmills[i].get_read_pins(self.wind_mode)
             states = self.get_states(checkpins)
-            next_states = self.windmills[i].next_rotating()
+            next_states = self.windmills[i].next_rotating(states)
             self.windmills[i].set_signal(next_states)
 
     
@@ -23,8 +25,11 @@ class Windmill_Calculator:
             return states
         for i in range(l):
             ind = pins[i]
-            s = int(self.windmills[ind].bRotating)
-            states.append(s)
+            if ind < 31:
+                s = self.windmills[ind].bRotating
+            else:
+                s = self.sensor_info[ind-31]
+            states.append(int(s))
         return states
 
     def changeMode(self):
